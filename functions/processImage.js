3c69c822d331ee13e4ae176e8e51af24a5e193c1
Yos Riady <yosriady@gmail.com>
2017-05-15 17:38:50 +0800

    Fix some issues

--- functions/processImage.js
+++ functions/processImage.js
@@ -8,7 +8,6 @@ module.exports.handler = (event, context, callback) => {
   console.log('Triggered by DB event');
   const { facialAnalysis, sceneAnalysis, id } = event.Records[0].dynamodb.NewImage;
   const fileName = id.S;
-  const emojiName = 'pigNose.png'
   const s3SourceKey = `${process.env.ORIGINAL_FOLDER_NAME}/${fileName}`;
   const s3DestinationKey = `${process.env.PROCESSED_FOLDER_NAME}/${fileName}`;
 
@@ -16,47 +15,23 @@ module.exports.handler = (event, context, callback) => {
   console.log(JSON.stringify(facialAnalysis));
   console.log(JSON.stringify(sceneAnalysis));
 
-  const downloadUserImagePromise = s3.getObject({ // Download image from S3
+  const params = { // Download image from S3
     Bucket: process.env.IMAGES_BUCKET_NAME,
     Key: s3SourceKey,
-  }).promise()
+  };
+  console.log(params);
+  return s3.getObject(params).promise()
     .catch(err => console.log(err.message))
     .then((data) => { // Write file to /tmp folder
-      console.log('downloadUserImage');
+      console.log(data);
       const destPath = path.join('/tmp', fileName);
-      console.log(`Writing downloaded user file to ${destPath}`);
+      console.log(`Writing downloaded file to ${destPath}`);
       fs.writeFileSync(destPath, data.Body);
       return destPath;
-    });
-  const downloadNoseImagePromise = s3.getObject({ // Download image from S3
-    Bucket: process.env.IMAGES_BUCKET_NAME,
-    Key: `emojis/${emojiName}`,
-  }).promise()
-    .catch(err => console.log(err.message))
-    .then((data) => { // Write file to /tmp folder
-      console.log('downloadNose');
-      const destPath = path.join('/tmp', emojiName);
-      console.log(`Writing downloaded nose file to ${destPath}`);
-      fs.writeFileSync(destPath, data.Body);
-      return destPath;
-    });
-
-  return Promise.all([
-    downloadUserImagePromise,
-    downloadNoseImagePromise,
-  ])
-    .then(([userImageFilePath, noseImageFilePath]) => { // TODO: Imagemagick code
-      console.log(userImageFilePath);
-      console.log(noseImageFilePath);
-      console.log(facialAnalysis);
-      // TODO: Read facialAnalysis
-      return sharp(userImageFilePath)
-        .overlayWith(noseImageFilePath, { gravity: sharp.gravity.southeast } )
-        .toBuffer();
-    })
-    .then((userImageFilePath) => { // Open processed file at userImageFilePath as buffer
-      console.log(`Opening file at ${userImageFilePath}`);
-      const fileBuffer = fs.readFileSync(userImageFilePath);
+    }) // TODO: imagemagick
+    .then((filePath) => { // Open processed file at filePath as buffer
+      console.log(`Opening file at ${filePath}`);
+      const fileBuffer = fs.readFileSync(filePath);
       return fileBuffer;
     })
     .then((buffer) => { // Save new processed image to S3
